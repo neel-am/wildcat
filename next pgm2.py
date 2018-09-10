c5980225bc19dc675b935de0b92568c77a2e2797
neel-am <42913321+neel-am@users.noreply.github.com>
2018-09-10 09:00:24 +0545

    Add files via upload

--- next pgm2.py
+++ next pgm2.py
@@ -1,6 +1,6 @@
 import random
 
-fruits = ("apples", "mangos", "grapes", "lychee", "banana")
+fruits = ("apples", "mangos", "grapes", "lychee", "banana", "berry")
 items = random.choice(fruits)
 print()
 
@@ -11,28 +11,43 @@ if game == "n":
 
 if game == "y":
     print("Please guess your option:")
-
+correct = []
+incorrect = []
 display = []
 display.extend(items)
 
 for i in range(len(display)):
     display[i] = "____"
+
+
 print(items)
 print(' '.join(display))
 print()
 
-count = 0
+count = 1
 
 while count < 10:
     guess = input("Please guess a letter:")
+
+    if guess in correct or guess in incorrect:
+        print("you have already guessed that letter guess again:")
+    elif len(guess) == 1:
+        print("please enter again")
+    else:
+        break
+    if guess in items:
+        correct.append(guess)
+    else:
+        incorrect.append(guess)
+
+    count = count + 1
     guess = guess.lower()
 
     print(guess)
     print(count)
-    count = count + 1
 
 
-    for i in range(len(items)):
+    for i in range(0, 6):
         if items[i] == guess:
             display[i] = guess
 
@@ -40,7 +55,10 @@ while count < 10:
             print()
 
 
-print("GOOD JOB!")
+
+print("Bye bye")
+
+
 
 
 
